92c2f9f210e251c527a3bf4851d4c0d2a2a92670
Milton <Milton.viot@gmail.com>
2021-12-07 19:12:06 -0300

    Tabela
    Tabela

---
+++ tabela.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE trb1(
+    
+    title varchar(10485760),
+    score varchar(10485760),
+    id varchar(10485760),
+    url varchar(10485760),
+    comms_num varchar(10485760),
+    created varchar(10485760),
+    body varchar(10485760),
+    data varchar(10485760)
+     
+); 
